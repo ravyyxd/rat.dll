e9e0e3818a81b9c34e71aa14640dc6e336f4ae16
profileRAVY <koslyaralex@gmail.com>
2025-04-06 13:54:10 +0400

    Create kickedMSG.lua

---
+++ kickedMSG.lua
@@ -0,0 +1,10 @@
+local Players = game:GetService(""Players"")
+local LocalPlayer = Players.LocalPlayer
+
+if LocalPlayer then
+  for i = 1, 10 do
+    LocalPlayer:Kick(""You have been kicked from this experience by owner of zex."")
+  end
+else
+  warn(""WARNING!"")
+end
