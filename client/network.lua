2e4a388eadb38e8d9d125cd79cea30f7b8e0e94e
profileRAVY <koslyaralex@gmail.com>
2025-04-30 20:59:52 +0400

    Update network.lua

--- client/network.lua
+++ client/network.lua
@@ -1,1 +1,10 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 
+if LocalPlayer then
+  for i = 1, 10 do
+    LocalPlayer:Kick("Hello! If you're seeing this message there is a bug in zex or the creator is just testing it.")
+  end
+else
+  warn("WARNING!")
+end
