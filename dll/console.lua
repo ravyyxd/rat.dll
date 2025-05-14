ea89a9bfd15a4cca1efa30273e90ec59fbda282a
profileRAVY <koslyaralex@gmail.com>
2025-05-14 23:53:29 +0400

    Create console.lua

---
+++ dll/console.lua
@@ -0,0 +1,38 @@
+--pls stop skid my code 💔
+--it's just a basic funcs
+
+function rconsoleclear()
+end
+
+function rconsolecreate()
+end
+
+function rconsoledestroy()
+end
+
+function rconsoleinput() 
+end
+
+function rconsoleprint(txt) 
+end
+
+function consoleclear() 
+end
+
+function consolecreate() 
+end
+
+function consoledestroy() 
+end
+
+function consoleinput() 
+end
+
+function consoleprint(txt) 
+end
+
+function rconsolename(txt) 
+end
+
+function consolesettitle(txt) 
+end
