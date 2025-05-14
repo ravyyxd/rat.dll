bf9d8e50439606be8c69b0315b3da16613adc855
profileRAVY <koslyaralex@gmail.com>
2025-05-14 23:59:39 +0400

    Create custom.lua

---
+++ dll/custom.lua
@@ -0,0 +1,27 @@
+--test funcs
+
+-- local exploit_identity = ...
+
+-- function getthreadidentity()  
+--    return exploit_identity
+-- end
+
+-- function getidentity() 
+--    return exploit_identity
+-- end
+
+-- function getthreadcontext() 
+--    return exploit_identity 
+-- end
+
+-- function setthreadidentity(identity) 
+--   return exploit_identity 
+-- end
+
+-- function setidentity(identity)
+--   return exploit_identity
+-- end
+
+-- function setthreadcontext(identity)
+--   return exploit_identity 
+-- end
