3199850b431912b138345e94c4114218d24be861
profileRAVY <koslyaralex@gmail.com>
2025-06-09 17:47:16 +0400

    Create test.cs

---
+++ client/test.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace File
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      System.Console.WriteLine("test v1.0")
+      Thread.Sleep(500); //system.threading;
+      System.Console.WriteLine("checking for zex....")
+    }
+  }
+}
